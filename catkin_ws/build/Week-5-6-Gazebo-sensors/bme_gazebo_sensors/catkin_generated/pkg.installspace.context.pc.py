485c5da5b9fb6d361ce07c32857d8d2b9bb5df6c
david.dudas <david.dudas@outlook.com>
2022-04-11 20:10:44 +0200

    cleaning up package

--- catkin_ws/build/Week-5-6-Gazebo-sensors/bme_gazebo_sensors/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "bme_gazebo_sensors"
-PROJECT_SPACE_DIR = "/home/toto/Documents/GitHub/Robotrendszerek-2021-22-2-Versenypalya/catkin_ws/install"
-PROJECT_VERSION = "0.0.1"
